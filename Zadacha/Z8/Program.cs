64261697b1ccaa91113a805d2bead609351eefde
Yulia Batrakova <julichka_71@mail.ru>
2022-01-04 23:59:35 +0300

    Z8 Added

---
+++ Zadacha/Z8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите большее число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите меньшее число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA < numberB)
+{
+    Console.WriteLine("не делится");
+}
+else
+{
+
+if (numberA % numberB == 0)
+{
+    Console.WriteLine($"{numberA} Кратно {numberB}");
+}
+else
+{
+    Console.Write("Не кратно. Остаток: ");
+    Console.WriteLine (numberA % numberB);
+}
+}
