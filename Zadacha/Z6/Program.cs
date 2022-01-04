a52edd2e03c38c1da83194ede9f857fdde48c3a2
Yulia Batrakova <julichka_71@mail.ru>
2022-01-04 23:07:12 +0300

    Z6 added

--- Zadacha/Z6/Program.cs
+++ Zadacha/Z6/Program.cs
@@ -1,2 +1,13 @@
-﻿int max = new Random().Next(10, 99);
-max = 0;
+﻿int max = new Random().Next(10, 100);
+Console.WriteLine(max);
+int numberA = max / 10;
+int numberB = max % 10;
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else 
+{
+    Console.WriteLine(numberB);
+}
+
