8908936fff03c5000317f6cb7468089148be61f2
Yulia Batrakova <julichka_71@mail.ru>
2022-01-05 00:29:18 +0300

    Z9 added

---
+++ Zadacha/Z9/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = new Random().Next(1, 10000);
+int Count = 0;
+int current = number;
+while (current > 0)
+{
+    current = current / 10;
+    Count ++;
+}
+Console.WriteLine(number);
+if (Count < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int third = number / 100 % 10;
+    Console.WriteLine(third);
+}
